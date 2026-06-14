ee6809d4ea4c27cadc3bad11c82768bf72d0d486
Sergey Zenkov <sergryzenkov@gmail.com>
2026-06-14 14:54:46 +0300

    replace worker

--- cmd/main.go
+++ cmd/main.go
@@ -3,14 +3,10 @@ package main
 import (
 	"TGbot/pkg/api"
 	"TGbot/pkg/db"
-	"bytes"
 	"context"
-	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 	"os"
-	"strings"
 	"time"
 
 	"github.com/jackc/pgx/v5/pgxpool"
@@ -18,84 +14,8 @@ import (
 )
 
 const (
-	urlMeteoMinsk = "https://api.open-meteo.com/v1/forecast?current=temperature_2m,relative_humidity_2m,weather_code,wind_speed_10m&latitude=53.9006&longitude=27.5590"
-	urlTG         = "https://api.telegram.org/bot"
-	urlFL         = "https://api.fantlab.ru/"
-	PSQLsourse    = "postgres://router_go:root@127.0.0.1:5432/router_go"
-	hiHelp        = `Бот может:
--при запросе /help дать подсказку как с ним взаимодействовать
--при запросе погоды сказать температуру и скорость ветра на улице в Минске (остальные города в разработке)
--дать информации о книге или авторе`
+	urlFL = "https://api.fantlab.ru/"
 )
-
-func GetUpdate(client *http.Client, url string, offset int) []api.Update {
-	reqUrl := fmt.Sprintf("%sgetUpdates?offset=%d&timeout=100", url, offset)
-	mes, err := client.Get(reqUrl)
-	if err != nil {
-		log.Println("error update: ", err)
-		return nil
-	}
-
-	defer mes.Body.Close()
-
-	var resp api.TelegramResponse
-	err = json.NewDecoder(mes.Body).Decode(&resp)
-	if err != nil {
-		log.Println("error decod: ", err)
-		return nil
-	}
-
-	return resp.Result
-}
-
-func SendMes(client *http.Client, url string, chat_id int, text string) {
-	body := api.MessBody{
-		ChatId: chat_id,
-		Text:   text,
-	}
-	byteBody, err := json.Marshal(body)
-	if err != nil {
-		log.Println(err)
-		return // Вместо log.Fatal делаем return
-	}
-	send, err := client.Post(url, "application/json", bytes.NewBuffer(byteBody))
-	if err != nil {
-		log.Println(err)
-		return // Вместо log.Fatal делаем return
-	}
-	defer send.Body.Close()
-	fmt.Println("Статус отправки:", send.StatusCode)
-}
-
-func WorkerBot(client *http.Client, mes <-chan api.Message) {
-	clientMeteo := &http.Client{
-		Timeout: 1000 * time.Second,
-	}
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal(err)
-	}
-	tokenTG := os.Getenv("tg")
-	for m := range mes {
-		mesText := strings.ToLower(m.Text)
-		switch {
-		case strings.Contains(mesText, "погода"):
-			askMeteo, err := clientMeteo.Get(urlMeteoMinsk)
-			if err != nil {
-				fmt.Println("ask error: ", err)
-			}
-			var bodyMeteo api.MeteoResponse
-			err = json.NewDecoder(askMeteo.Body).Decode(&bodyMeteo)
-			if err != nil {
-				fmt.Println("json Meteo error: ", err)
-			}
-			SendMes(client, urlTG+tokenTG+"/sendMessage", m.From.Id, fmt.Sprintf("Температура воздуха в минске %.2f%s, сторость ветра %.2f%s", bodyMeteo.Current.Temperature, bodyMeteo.CurrentUnits.Temperature, bodyMeteo.Current.Wind, bodyMeteo.CurrentUnits.Wind))
-			SendMes(client, urlTG+tokenTG+"/sendMessage", m.From.Id, fmt.Sprintf("Текущее время %s по UTC", bodyMeteo.Current.Time))
-		case strings.Contains(mesText, "/help"):
-			SendMes(client, urlTG+tokenTG+"/sendMessage", m.From.Id, hiHelp)
-		}
-	}
-}
 
 func main() {
 	client := &http.Client{
@@ -106,6 +26,8 @@ func main() {
 		log.Fatal(err)
 	}
 	tokenTG := os.Getenv("tg")
+
+	PSQLsourse := os.Getenv("PSQLsourse")
 	offset := 0
 	//PSQLsourse = "postgres://router_go:root@127.0.0.1:5432/router_go"
 	dbpool, err := pgxpool.New(context.Background(), PSQLsourse)
@@ -118,11 +40,11 @@ func main() {
 	dbChan := make(chan api.Message, 20)
 	botChan := make(chan api.Message, 20)
 
-	go WorkerBot(client, botChan)
+	go api.WorkerBot(client, botChan)
 	go db.WorkerDB(dbpool, dbChan)
 
 	for {
-		upd := GetUpdate(client, urlTG+tokenTG+"/", offset)
+		upd := api.GetUpdate(client, api.UrlTG+tokenTG+"/", offset)
 
 		for _, update := range upd {
 			if update.Message.Text != "" {
--- pkg/api/tg.go
+++ pkg/api/tg.go
@@ -1,5 +1,27 @@
 package api
 
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/joho/godotenv"
+)
+
+const (
+	UrlTG  = "https://api.telegram.org/bot"
+	hiHelp = `Бот может:
+-при запросе /help дать подсказку как с ним взаимодействовать
+-при запросе погоды сказать температуру и скорость ветра на улице в Минске (остальные города в разработке)
+-дать информации о книге или авторе`
+	urlMeteoMinsk = "https://api.open-meteo.com/v1/forecast?current=temperature_2m,relative_humidity_2m,weather_code,wind_speed_10m&latitude=53.9006&longitude=27.5590"
+)
+
 type MessBody struct {
 	ChatId int    `json:"chat_id"`
 	Text   string `json:"text"`
@@ -26,4 +48,72 @@ type Update struct {
 type TelegramResponse struct {
 	Ok     bool     `json:"ok"`
 	Result []Update `json:"result"`
+}
+
+func GetUpdate(client *http.Client, url string, offset int) []Update {
+	reqUrl := fmt.Sprintf("%sgetUpdates?offset=%d&timeout=100", url, offset)
+	mes, err := client.Get(reqUrl)
+	if err != nil {
+		log.Println("error update: ", err)
+		return nil
+	}
+
+	defer mes.Body.Close()
+
+	var resp TelegramResponse
+	err = json.NewDecoder(mes.Body).Decode(&resp)
+	if err != nil {
+		log.Println("error decod: ", err)
+		return nil
+	}
+
+	return resp.Result
+}
+
+func SendMes(client *http.Client, url string, chat_id int, text string) {
+	body := MessBody{
+		ChatId: chat_id,
+		Text:   text,
+	}
+	byteBody, err := json.Marshal(body)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	send, err := client.Post(url, "application/json", bytes.NewBuffer(byteBody))
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer send.Body.Close()
+	fmt.Println("Статус отправки:", send.StatusCode)
+}
+func WorkerBot(client *http.Client, mes <-chan Message) {
+	clientMeteo := &http.Client{
+		Timeout: 1000 * time.Second,
+	}
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+	tokenTG := os.Getenv("tg")
+	for m := range mes {
+		mesText := strings.ToLower(m.Text)
+		switch {
+		case strings.Contains(mesText, "погода"):
+			askMeteo, err := clientMeteo.Get(urlMeteoMinsk)
+			if err != nil {
+				fmt.Println("ask error: ", err)
+			}
+			var bodyMeteo MeteoResponse
+			err = json.NewDecoder(askMeteo.Body).Decode(&bodyMeteo)
+			if err != nil {
+				fmt.Println("json Meteo error: ", err)
+			}
+			SendMes(client, UrlTG+tokenTG+"/sendMessage", m.From.Id, fmt.Sprintf("Температура воздуха в минске %.2f%s, сторость ветра %.2f%s", bodyMeteo.Current.Temperature, bodyMeteo.CurrentUnits.Temperature, bodyMeteo.Current.Wind, bodyMeteo.CurrentUnits.Wind))
+			SendMes(client, UrlTG+tokenTG+"/sendMessage", m.From.Id, fmt.Sprintf("Текущее время %s по UTC", bodyMeteo.Current.Time))
+		case strings.Contains(mesText, "/help"):
+			SendMes(client, UrlTG+tokenTG+"/sendMessage", m.From.Id, hiHelp)
+		}
+	}
 }
